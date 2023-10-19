3cb28c63330ff1cf1e2f157ba5f887400de7451b
cbh778899 <cbh778899@outlook.com>
2023-10-19 17:30:59 +1100

    implement login & bug fix

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 
+// imports
 const fs = require('fs')
 const exec = require('child_process').exec
-const TYPES = require('./types')
 
-fs.existsSync(`./${TYPES.FILES_DIR}`) || fs.mkdirSync(`./${TYPES.FILES_DIR}`)
-fs.existsSync(`./${TYPES.PROCESS_DIR}`) || fs.mkdirSync(`./${TYPES.PROCESS_DIR}`)
+// types
+const PROCESS_DIR = 'waiting_process',
+      FILES_DIR = 'all_files',
+      SPECIAL_TRUNK_START = 'start-chunk',
+      SPECIAL_TRUNK_END = 'end-chunk';
 
+// init
+require('./database').openDB(null, true);
+fs.existsSync(`./${FILES_DIR}`) || fs.mkdirSync(`./${FILES_DIR}`)
+fs.existsSync(`./${PROCESS_DIR}`) || fs.mkdirSync(`./${PROCESS_DIR}`)
+
+// express init
 const express = require('express');
 const bodyParser = require('body-parser');
+const { login } = require('./actions/account_actions');
 const app = express();
 app.use(require('cors')())
+app.use(bodyParser.json())
 
 const router = express.Router()
 
@@ -18,8 +29,13 @@ router.get('/', (req, res)=>{
     res.sendFile(`${__dirname}/client.html`)
 })
 
+router.post('/login', async (req, res) => {
+    const { account, password } = req.body;
+    res.json(await login(account, password));
+})
+
 router.get('/playlists', (req, res) => {
-    const playlists = fs.readdirSync(`./${TYPES.FILES_DIR}`, {withFileTypes: true})
+    const playlists = fs.readdirSync(`./${FILES_DIR}`, {withFileTypes: true})
         .filter(f=>f.isDirectory())
         .map(e=>e.name)
     res.json(playlists)
@@ -40,12 +56,12 @@ router.post('/upload-file', bodyParser.raw({type: 'application/octet-stream', li
     const { filename, isSpecialTrunk, fileType, renameFile } = req.query;
     const data = req.body;
     
-    const process_file = `./${TYPES.PROCESS_DIR}/${filename}`
-    isSpecialTrunk === TYPES.SPECIAL_TRUNK_START && fs.existsSync(process_file) && fs.unlinkSync(process_file);
+    const process_file = `./${PROCESS_DIR}/${filename}`
+    isSpecialTrunk === SPECIAL_TRUNK_START && fs.existsSync(process_file) && fs.unlinkSync(process_file);
     fs.appendFileSync(process_file, data)
-    if(isSpecialTrunk === TYPES.SPECIAL_TRUNK_END) {
+    if(isSpecialTrunk === SPECIAL_TRUNK_END) {
         let playlist_name = renameFile || filename.split('.').slice(0, -1).join('.')
-        const playlist_dir = `./${TYPES.FILES_DIR}/${playlist_name}`
+        const playlist_dir = `./${FILES_DIR}/${playlist_name}`
         fs.existsSync(playlist_dir) || fs.mkdirSync(playlist_dir)
         exec(`ffmpeg -i "${process_file}" ${fileType === 'video' ? '-map 0:v ' : ''}-map 0:a ${fileType === 'video' ? '-c:v libx264 ' : ''}-c:a aac -start_number 0 -hls_time 10 -hls_list_size 0 -f hls "${playlist_dir}/${playlist_name}.m3u8"`, (err, stdout, stderr) => {
             if(err) {
