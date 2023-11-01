a173c7a564f8691eaffe04c289873e46809c3304
cbh778899 <cbh778899@outlook.com>
2023-11-01 21:52:19 +1100

    add route to src folder

--- routes/global.js
+++ routes/global.js
@@ -5,5 +5,10 @@ module.exports = (app, express, dirName) => {
         res.sendFile(`${dirName}/client.html`)
     })
 
+    router.get('/src/:filename', (req, res) => {
+        const {filename} = req.params;
+        res.sendFile(`${dirName}/src/${filename}`)
+    })
+
     app.use('/', router)
 }
